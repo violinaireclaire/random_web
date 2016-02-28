b1b1ad648114acac551df44b8d7d8c483aaca320
Mansibunny <myyoga75@yahoo.com>
2016-02-27 19:21:27 -0500

    got stuff working

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
-	$('#firstInput').val(3)
+
+	var ones=0;
+	var nums = ["1","1","1","1","1","1","1","1","1","1",
+				"1","1","1","1","1","1","1","1","1","1",
+				"1","1","1","1","1","1","1","1","1","1",
+				"1","1","1","1","1","1","1","1","1","1",
+				"1","1","1","1","1","1","1","1","1","1",
+				"1","1", "1"]
+
+	$("#submitButton").click(function(){
+
+		//check each box
+		var allValid = 1
+		$("input").each(function(i) {
+
+			if(this.value.localeCompare(nums[i])===0) {
+				console.log("goodstuff");
+			}
+			else{
+				allValid=0
+			}
+
+		});
+
+		//if all valid, then print 
+		if(allValid===1){
+			alert("goodstuff!!");
+		}
+		else{
+			alert("noooo :(");
+		}
+
+	});
+
 })
