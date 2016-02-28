5756683c262089a42a1d0da77ae4b5e49ca9e293
Mansibunny <myyoga75@yahoo.com>
2016-02-27 22:59:13 -0500

    I finally got it!

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 $(document).ready(function(){
 
 	var ones=0;
-	var nums = ["1","1","1","1","1","1","1","1","1","1",
-				"1","1","1","1","1","1","1","1","1","1",
-				"1","1","1","1","1","1","1","1","1","1",
-				"1","1","1","1","1","1","1","1","1","1",
-				"1","1","1","1","1","1","1","1","1","1",
-				"1","1", "1"]
-
+	var nums = ["4","9","6","5","1","3","2","1","9","2",
+				"5","5","3","4","6","1","8","7","5","2",
+				"1","3","8","1","6","5","8","3","7","9",
+				"3","7","9","4","1","3","9","6","2","8",
+				"5","1","8","1","9","2","2","4","1","7",
+				"9","6","8"]
 	$("#submitButton").click(function(){
 
 		//check each box
 		var allValid = 1
+		var count=0;
 		$("input").each(function(i) {
 
 			if(this.value.localeCompare(nums[i])===0) {
-				console.log("goodstuff");
 			}
 			else{
 				allValid=0
 			}
+			if (i == 51)
+			{
+				if(allValid===1){
+					alert("goodstuff!!");
+				}
+				else{
+					alert("noooo :(");
+				}
+
+			}
 
 		});
-
-		//if all valid, then print 
-		if(allValid===1){
-			alert("goodstuff!!");
-		}
-		else{
-			alert("noooo :(");
-		}
 
 	});
 
